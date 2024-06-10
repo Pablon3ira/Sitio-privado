30eed44c0cac4435bc9fff9025bf2f0ea6c3afdd
Pablo <juanpablo.neira@estudioenpenascal.com>
2024-06-10 13:20:18 +0200

    Commit 3

---
+++ js/home.js
@@ -0,0 +1,31 @@
+function GetCharacters(done) {
+    const results = fetch("https://rickandmortyapi.com/api/character/?page=19");
+
+    results
+        .then(response => response.json())
+        .then(data => {
+            done(data)
+        });
+}
+
+GetCharacters(data => (
+    data.results.forEach(personaje => {
+
+        const article = document.createRange().createContextualFragment(/*html*/`
+       <article>
+       <div class="image-container">
+       <img src="${personaje.image}" alt="Personaje">
+      </div>
+      <h2>${personaje.name}</h2>
+      <span>${personaje.status}</span>
+      </article>
+ `);
+
+        const main = document.querySelector("main");
+        main.append(article);
+
+    })
+     
+    
+    ));
+    
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Definimos los datos correctos de acceso
-const correctUsername = "admin";
+const correctUsername = "admin@admin";
 const correctPassword = "password123";
 
 // Función para verificar los datos de acceso
@@ -19,20 +19,13 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
     if (verifyLogin(username, password)) {
         alert("Acceso permitido!");
         // Redirigir o realizar alguna acción adicional aquí
+        //insertar codigo que navegue HREF JS
+        
     } else {
         document.getElementById('errorMsg').innerText = "Invalid username or password.";
-        
-    }
-    
-});
-document.getElementById('loginForm').addEventListener('submit', function (event) {
-    var termsCheckbox = document.getElementById('terms');
 
-    if (!termsCheckbox.checked) {
-        alert('Debes aceptar los términos y condiciones para continuar.');
-        event.preventDefault(); // Evita el envío del formulario
     }
+
 });
 
-//window.alert("Failed access");
-  //  alert("Acceso denegado!");
+
